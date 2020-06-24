e06e2f2df75e7ce12d8fd0fbf40ebb5f7e95c2bd
7h3730B <teoschulze5@gmail.com>
2020-06-24 19:03:48 +0200

    :sparkles: Coded Episode 1

--- Discord.js/Episode-1/main.js
+++ Discord.js/Episode-1/main.js
@@ -1,7 +1,45 @@
 const Discord = require('discord.js');
-const config = require('./config.json');
+// uses destructuring to extract the prefix and token from your config file.
+const { token, prefix } = require('./config.json');
 
 const client = new Discord.Client();
 
+// Gets called when the Bot is ready
+client.on('ready', () => {
+	console.log(`Logged in as ${client.user.username}...`);
+});
+
+const cmdmap = {
+	say: CmdSay,
+	test: CmdTest
+};
+
+function CmdSay(msg, args) {
+	msg.channel.send(args.join(' '));
+}
+
+function CmdTest(msg, args) {
+	console.log('test');
+}
+
+// Gets called when the Bot receives a message.
+// The msg Object is given to us by the discord.js libary.
+// When you want to know what you can do with the msg Object
+// look in the docs here: https://discord.js.org/#/docs/main/stable/class/Message
+client.on('message', (msg) => {
+	// if the author of the message is a bot
+	// and if the message doesn't start with the prefix in your config.json the bot returns.
+	if (msg.author.bot || !msg.content.startsWith(prefix)) return;
+
+	// removes the first x letters and splits the message be every space
+	const args = msg.content.slice(prefix.length).split(/ +/);
+	// gets the first argument from args and transforms it to lower case (e.g. PiNG => ping)
+	const invoke = args.shift().toLowerCase();
+
+	if (invoke in cmdmap) {
+		cmdmap[invoke](msg, args);
+	}
+});
+
 // uses the token in your config.json to log into your Bot
-client.login(config.token);
+client.login(token);
